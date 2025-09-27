88565181425338bc10657b0338df10f5f1e5d629
SC Lee <jenls.bb@gmail.com>
2025-09-27 16:53:54 -0400

    implement text to speech method

--- worker.py
+++ worker.py
@@ -30,7 +30,30 @@ def speech_to_text(audio_binary):
 
 
 def text_to_speech(text, voice=""):
-    return None
+    def text_to_speech(text, voice=""):
+    # Set up Watson Text-to-Speech HTTP Api url
+    base_url = "https://sn-watson-stt.labs.skills.network"
+    api_url = base_url + '/text-to-speech/api/v1/synthesize?output=output_text.wav'
+
+    # Adding voice parameter in api_url if the user has selected a preferred voice
+    if voice != "" and voice != "default":
+        api_url += "&voice=" + voice
+
+    # Set the headers for our HTTP request
+    headers = {
+        'Accept': 'audio/wav',
+        'Content-Type': 'application/json',
+    }
+
+    # Set the body of our HTTP request
+    json_data = {
+        'text': text,
+    }
+
+    # Send a HTTP Post request to Watson Text-to-Speech Service
+    response = requests.post(api_url, headers=headers, json=json_data)
+    print('text to speech response:', response)
+    return response.content
 
 
 def openai_process_message(user_message):
