adc85787abc68f32225b15f98cd72acb6d5c9f6a
SC Lee <jenls.bb@gmail.com>
2025-09-27 17:15:32 -0400

    implement the message process loop

--- server.py
+++ server.py
@@ -33,12 +33,33 @@ def speech_to_text_route():
 
 
 @app.route('/process-message', methods=['POST'])
-def process_prompt_route():
+def process_message_route():
+    user_message = request.json['userMessage'] # Get user's message from their request
+    print('user_message', user_message)
+
+    voice = request.json['voice'] # Get user's preferred voice from their request
+    print('voice', voice)
+
+    # Call openai_process_message function to process the user's message and get a response back
+    openai_response_text = openai_process_message(user_message)
+
+    # Clean the response to remove any emptylines
+    openai_response_text = os.linesep.join([s for s in openai_response_text.splitlines() if s])
+
+    # Call our text_to_speech function to convert OpenAI Api's reponse to speech
+    openai_response_speech = text_to_speech(openai_response_text, voice)
+
+    # convert openai_response_speech to base64 string so it can be sent back in the JSON response
+    openai_response_speech = base64.b64encode(openai_response_speech).decode('utf-8')
+
+    # Send a JSON response back to the user containing their message's response both in text and speech formats
     response = app.response_class(
-        response=json.dumps({"openaiResponseText": None, "openaiResponseSpeech": None}),
+        response=json.dumps({"openaiResponseText": openai_response_text, "openaiResponseSpeech": openai_response_speech}),
         status=200,
         mimetype='application/json'
     )
+
+    print(response)
     return response
 
 
